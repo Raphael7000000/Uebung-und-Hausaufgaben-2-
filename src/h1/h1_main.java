ad001d64d149f497c5e2187d6395594d68c59fc6
Raphael <rgeier07@gmail.com>
2025-10-29 18:28:16 +0100

    Hasuaufgaben

--- src/h1/h1_main.java
+++ src/h1/h1_main.java
@@ -1,5 +1,11 @@
 package h1;
 
 public class h1_main {
-
+public static void main(String[] args) {
+	
+	int i = 11;
+	if (i>10) {
+		i = i-10;
+	}
+}
 }
--- src/h2/h2_main.java
+++ src/h2/h2_main.java
@@ -1,5 +1,50 @@
 package h2;
 
 public class h2_main {
-
-}
+	public static void main(String[] args) {
+ int i = 13;
+ int j = 16;
+ int k = 12;
+ int min = 0;
+ int max = 0;
+ 
+ if (i<j) {
+	 if(i<k) {
+		 
+		 min = i;		 
+	 }
+ }
+ if (k<j) {
+	 if(k<i) {
+		 
+		 min = k;		 
+	 }
+ }
+ if (j<k) {
+	 if(j<i) {
+		 
+		 min = j;		 
+	 }
+ }
+ if (i>j) {
+	 if(i>k) {
+		 
+		 max = i;		 
+	 }
+ }
+ if (k>j) {
+	 if(k>i) {
+		 
+		 max = k;		 
+	 }
+ }
+ if (j>k) {
+	 if(j>i) {
+		 
+		 max = j;		 
+	 }
+ }
+ 
+	
+	
+	}}
